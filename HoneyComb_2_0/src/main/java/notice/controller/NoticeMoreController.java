fcf85afd3469e6e95413fe9f5c4cea04c8716355
mia <mia@DESKTOP-H1FH6FJ>
2016-09-23 14:11:21 +0900

    선화 : notice controller dao setter add

--- HoneyComb_2_0/src/main/java/notice/controller/NoticeMoreController.java
+++ HoneyComb_2_0/src/main/java/notice/controller/NoticeMoreController.java
@@ -5,10 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.LocaleResolver;
 
+import notice.db.NoticeDao;
+
 @Controller
 @RequestMapping("/more")
 public class NoticeMoreController {
 
+	private NoticeDao dao;
+
+	public void setDao(NoticeDao dao) {
+		this.dao = dao;
+	}
+	
 	private LocaleResolver localeResolver;
 
 	@RequestMapping(method = RequestMethod.GET)
