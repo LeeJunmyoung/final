acee3ae16427620bd038e9bbe0c8a80cd852c84d
user1 <user1@user1-PC>
2016-09-23 14:51:51 +0900

    규호:클라우드 연결 셋팅

--- HoneyComb_2_0/WebContent/index.jsp
+++ HoneyComb_2_0/WebContent/index.jsp
@@ -8,6 +8,6 @@
 	<!-- <a href="login/test.do">test.do</a> -->
 	<a href="main/main">main</a>
 	<a href="notice/more">notice/more</a>
-
+	<a href="cloud/cloud">cloud</a>
 </body>
 </html>
--- HoneyComb_2_0/src/main/java/cloud/controller/CloudMainController.java
+++ HoneyComb_2_0/src/main/java/cloud/controller/CloudMainController.java
@@ -2,16 +2,24 @@ package cloud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import cloud.db.CloudDao;
 
 @Controller
+@RequestMapping("/cloud")
 public class CloudMainController {
-	CloudDao dao;
+	private CloudDao dao;
 	
 	@Autowired
 	public void setDao(CloudDao dao) {
 		this.dao = dao;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String startMethod(){
+		return "cloud_main";
 	}
 	
 }
