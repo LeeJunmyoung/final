f8ea1273826d39c251ce9d106c4791883ae6bbfd
Administrator <Administrator@MSDN-SPECIAL>
2016-09-26 13:52:46 +0900

    동현 : xml수정 및 admin 회사확인

--- HoneyComb_2_0/src/main/java/admin/db/adminDao.java
+++ HoneyComb_2_0/src/main/java/admin/db/adminDao.java
@@ -1,7 +1,19 @@
 package admin.db;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 
-public class adminDao extends SqlSessionDaoSupport{
+import admin.db.adminInfo;
 
+public class adminDao extends SqlSessionDaoSupport {
+
+	public List<adminInfo> getadminList(int com_num) {
+		List adminlist = new ArrayList<adminInfo>();
+		adminlist = getSqlSession().selectList("admin.getadminlist", com_num);
+
+		return adminlist;
+
+	}
 }
