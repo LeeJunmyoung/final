dcc8a5071c40bf19a5a2316a0807f1f2e5e6a793
user1 <user1@user1-PC>
2016-09-27 15:07:46 +0900

    경민 SignInController

--- HoneyComb_2_0/src/main/java/login/controller/SignInController.java
+++ HoneyComb_2_0/src/main/java/login/controller/SignInController.java
@@ -1,20 +1,48 @@
 package login.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.LocaleResolver;
-
-
-
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/signIn.do")
 public class SignInController {
-
-
-
-		@RequestMapping(method = RequestMethod.GET)
+	
+	private static final Logger log = LoggerFactory.getLogger(SignInController.class);
+	
+	@Autowired
+	SignInDAO dao;
+	
+	public void setDao(SignInDAO dao) {
+		this.dao = dao;
+	}
+	@RequestMapping("/signIn.do")//signIn 폼으로 가는 메소드 
+	public String signInForm(){
+		
+		return "signIn";
+	}
+	@RequestMapping("/members.do")//DB에 insert하는 메소드
+	public ModelAndView members(LogOnDataBean logdb,HttpSession session){
+		
+		ModelAndView mav= new ModelAndView();
+		
+		return mav;
+	}
+	/*@RequestMapping("/overlaptest.do")//email 중복확인 하는 메소드
+	@ResponseBody
+	public String overlapTest(String email){
+		
+		String checkEmail = dao.overlapTest(email);
+		return checkEmail;
+	}*/
+		/*@RequestMapping(method = RequestMethod.GET)
 		public String form() {
 			return "signIn";
 		}
@@ -23,13 +51,9 @@ public class SignInController {
 		public String submit() {
 			
 			
-				return "signIn";
-				
-				
-				
+				return "signIn";*/						
 		}
 
 	
 	
 	
-}
