08ddaa65cc94f975029aeca647b1cbe7f86c1f37
Administrator <Administrator@MSDN-SPECIAL>
2016-09-27 10:28:11 +0900

    동현 : 회사확인중

---
+++ HoneyComb_2_0/src/main/java/admin/controller/admin_comSelect.java
@@ -0,0 +1,38 @@
+package admin.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import admin.db.adminDao;
+
+@Controller
+@RequestMapping("/admin_comSelect")
+public class admin_comSelect {
+
+	private adminDao dao;
+
+	@Autowired
+	public void setDao(adminDao dao) {
+		this.dao = dao;
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView getadminList(HttpSession session, HttpServletRequest request) {
+		int com_num = 0;
+		session = request.getSession();
+		session.setAttribute("com_num", 1);
+		com_num = (int) request.getSession().getAttribute("com_num");
+		List adminlist = dao.getadminList(com_num);
+		ModelAndView mav = new ModelAndView("admin_main", "adminlist", adminlist);
+		return mav;
+	}
+
+}
