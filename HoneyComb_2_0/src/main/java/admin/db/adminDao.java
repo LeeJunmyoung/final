7f05b60f6b0340b6805b68d78cf9d59e2c4201eb
Administrator <Administrator@MSDN-SPECIAL>
2016-09-27 13:27:27 +0900

    동현 : 회사확인 임시완성

--- HoneyComb_2_0/src/main/java/admin/db/adminDao.java
+++ HoneyComb_2_0/src/main/java/admin/db/adminDao.java
@@ -10,14 +10,9 @@ import admin.db.adminInfo;
 public class adminDao extends SqlSessionDaoSupport {
 
 	public List<adminInfo> getadminList(int com_num) {
-		System.out.println("getadminList실행중");
 		List adminlist = new ArrayList<adminInfo>();
-		System.out.println(adminlist);
-		System.out.println("getadminList중간");
 		adminlist = getSqlSession().selectList("getadminlist", com_num);
 		System.out.println(adminlist);
-		System.out.println("getadminList마지막");
-
 		return adminlist;
 
 	}
