e99abbd62bf85365ee4a809b9238539f11679a2a
user1 <user1@user1-PC>
2016-10-05 09:24:47 +0900

    준명 : cal 수정중

--- HoneyComb_2_0/src/main/java/cal/controller/CalMainController.java
+++ HoneyComb_2_0/src/main/java/cal/controller/CalMainController.java
@@ -1,21 +1,45 @@
 package cal.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import cal.db.Cal_DAO;
+
 @Controller
 @RequestMapping("/cal_main.do")
 public class CalMainController {
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public String calMain(){
-		
-		System.out.println("cal main 실행잼");
-		
+	private Cal_DAO dao;
+	
+	
+	
+	public void setDao(Cal_DAO dao) {
+		this.dao = dao;
+	}
 
-		return "cal_main";	
+
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView calMain(HttpServletRequest request){
+		
+		int mem_num =(int) request.getSession().getAttribute("mem_num");
+		
+		ModelAndView mav = new ModelAndView("cal_more");
+		
+		List totalCal =  dao.viewCal(mem_num);
+		
+		mav.addObject("totalCal", totalCal);
+		mav.addObject("cal_count",totalCal.size());
+		
+		
+		return mav;	
 	}
 
 }
--- HoneyComb_2_0/src/main/java/cal/db/Cal_DAO.java
+++ HoneyComb_2_0/src/main/java/cal/db/Cal_DAO.java
@@ -1,6 +1,9 @@
 package cal.db;
 
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.mybatis.spring.support.SqlSessionDaoSupport;
@@ -35,7 +38,32 @@ public class Cal_DAO extends SqlSessionDaoSupport{
 		
 		
 	}
-	
+
+	public List viewCal(int mem_num) {
+
+		List totalCal = new ArrayList<>();
+		List totalCal1 = new ArrayList<>();
+		
+		totalCal = getSqlSession().selectList("cal.viewCal", mem_num);
+		
+		for (Object c : totalCal) {
+			Cal_DataBean cal = (Cal_DataBean) c;
+			
+			Date formatdate = new Date(cal.getCal_end().getYear(),cal.getCal_end().getMonth(),cal.getCal_end().getDate()+1);
+			
+			
+			cal.setCal_end(formatdate);
+			formatdate= new Date(cal.getCal_start().getYear(),cal.getCal_start().getMonth(),cal.getCal_start().getDate());
+			cal.setCal_start(formatdate);
+			//System.out.println(cal.toString());
+			
+
+			totalCal1.add(cal);
+		}
+
+		return totalCal1;
+
+	}
 	
 	
 }
